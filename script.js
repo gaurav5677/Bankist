c32a017dc7f8a851052be5865a0c885426bb4016
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2022-12-21 18:31:00 +0530

    DOM manipularion using html Script

--- script.js
+++ script.js
@@ -61,6 +61,20 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = " "; // innterHtml is bit similar to text context . innterHTML return everything including the html , but here  it is used as a setter
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? "deposit" : "withdrawal";
+
+    const html = `<div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+    <div class="movements__value">${mov}</div>
+ </div>
+ `;
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
+};
+displayMovements(account1.movements);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
